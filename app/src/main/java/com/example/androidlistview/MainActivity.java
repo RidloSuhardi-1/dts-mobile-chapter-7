f16d7c6ec4cdbad46487c86161e0db1e05668baa
Ridlo Shuhardi <62533910+RidloSuhardi-1@users.noreply.github.com>
2021-08-10 18:51:19 +0700

    tampil ibukota di toast

--- app/src/main/java/com/example/androidlistview/MainActivity.java
+++ app/src/main/java/com/example/androidlistview/MainActivity.java
@@ -11,6 +11,8 @@ import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import org.json.JSONArray;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -18,23 +20,30 @@ public class MainActivity extends AppCompatActivity {
 
     private ListView lvItem;
     private String[] namaNegara = new String[] { "Indonesia", "Malaysia", "Singapura", "Italia", "Inggris", "Belanda", "Argentina", "Chile", "Mesir", "Uganda" };
+    private String[] namaIbuKota = new String[] { "Jakarta", "Kuala Lumpur", "Singapura", "Roma", "London", "Amsterdam", "Buenos Aires", "Santiago", "Cairo", "Kampala" };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        getSupportActionBar().setTitle("Aplikasi ListView Sederhana");
+        getSupportActionBar().setTitle("Aplikasi ListView");
+
+        ArrayList<Negara> listNegara = new ArrayList<Negara>();
+
+        for (int i = 0; i < namaNegara.length; i++) {
+            Negara n = new Negara(namaNegara[i], namaIbuKota[i]);
+            listNegara.add(n);
+        }
 
         lvItem = (ListView)findViewById(R.id.list_view);
-
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(MainActivity.this, R.layout.view_item_negara, R.id.txtNegara, namaNegara);
 
         lvItem.setAdapter(adapter);
         lvItem.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Toast.makeText(MainActivity.this, "Negara yang dipilih : " + namaNegara[position], Toast.LENGTH_LONG).show();
+                Toast.makeText(MainActivity.this, "Negara yang dipilih : " + listNegara.get(position).getNegara() + ", " + listNegara.get(position).getIbuKota(), Toast.LENGTH_LONG).show();
             }
         });
     }
--- app/src/main/java/com/example/androidlistview/Negara.java
+++ app/src/main/java/com/example/androidlistview/Negara.java
@@ -1,12 +1,20 @@
 package com.example.androidlistview;
 
 public class Negara {
-    private String negara;
-    private String ibuKota;
+    public String negara;
+    public String ibuKota;
 
     public Negara(String negara, String ibuKota) {
         this.negara = negara;
         this.ibuKota = ibuKota;
+    }
+
+    public String getNegara() {
+        return negara;
+    }
+
+    public String getIbuKota() {
+        return ibuKota;
     }
 
 }
