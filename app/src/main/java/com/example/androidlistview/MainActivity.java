06e709bfd5ecb70e4454acb632665ac7bedef930
Ridlo Shuhardi <62533910+RidloSuhardi-1@users.noreply.github.com>
2021-08-11 15:39:20 +0700

    Tambah teks ibukota pada toast

--- app/src/main/java/com/example/androidlistview/MainActivity.java
+++ app/src/main/java/com/example/androidlistview/MainActivity.java
@@ -43,7 +43,7 @@ public class MainActivity extends AppCompatActivity {
         lvItem.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Toast.makeText(MainActivity.this, "Negara yang dipilih : " + listNegara.get(position).getNegara() + ", " + listNegara.get(position).getIbuKota(), Toast.LENGTH_LONG).show();
+                Toast.makeText(MainActivity.this, "Negara yang dipilih : " + listNegara.get(position).getNegara() + ", Ibu Kota : " + listNegara.get(position).getIbuKota(), Toast.LENGTH_LONG).show();
             }
         });
     }
